97c7f99738af505a34cc7fca6abdea01ccf83bd5
estherxaviour <estherxviour@gmail.com>
2016-02-06 20:06:52 +0530

    Javascript Project for Session 5 and Session6

---
+++ Project_HTML,CSS,JavaScript/models/actor.js
@@ -0,0 +1,16 @@
+var imdb = imdb || {};
+if(!imdb._currentID){
+imdb._currentID = 1;
+}
+(function moduleActor(imdb){
+var Actor = function(name,bio,born){
+this._id = imdb._currentID;
+imdb._currentID++;
+this.name = name;
+this.bio = bio;
+this.born = born;
+}
+imdb.getActor = function getActor(name,bio,born){
+return new Actor(name,bio,born);
+}
+})(imdb);
---
+++ Project_HTML,CSS,JavaScript/models/review.js
@@ -0,0 +1,16 @@
+var imdb = imdb || {};
+if(!imdb._currentID){
+imdb._currentID = 1;
+}
+(function moduleReview(imdb){
+var Review = function(author,content,date){
+	this._id = imdb._currentID;
+	imdb._currentID++;
+	this.author = author;
+	this.content = content;
+	this.date = date;
+}
+imdb.getReview = function getReview(author,content,date){
+	return new Review(author,content,date);
+}
+})(imdb);
